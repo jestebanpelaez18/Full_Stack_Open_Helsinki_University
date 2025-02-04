4e9eb9fc75779f8e702bd97f959eeccf05420540
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-02-04 18:19:34 +0200

    feat(unicafe): Added exercise 1.7 of Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,18 +22,41 @@ const Header = (props) => {
   )
 }
 
+const Average = (props) => {
+  const average = props.puntuation / props.total
+  return (
+    <p>average {average}</p>
+  )
+}
+
+const Positive = (props) => {
+  const positive = props.good / props.total * 100
+  return (
+    <p>positive {positive} %</p>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [total,setTotal] = useState(0)
+  const [puntuation,setPuntuation] = useState(0)
+  
   const HandleGood = () => {
-    console.log("pressed Good")
-    console.log(good)
-    setGood(good + 1)}
-  const HandleNeutral = () => setNeutral(neutral + 1)
-  const HandleBad = () => setBad(bad + 1)
+    setGood(good + 1)
+    setTotal(total + 1)
+    setPuntuation(puntuation + 1)}
+  const HandleNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)}
+  const HandleBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setPuntuation(puntuation - 1)}
+  
+
   return (
     <div>
       <Header text="give feedback"></Header>
@@ -44,6 +67,9 @@ const App = () => {
       <Display text="good" counter={good}/>
       <Display text="neutral" counter={neutral}/>
       <Display text="bad" counter={bad}/>
+      <Display text="all" counter={total}/>
+      <Average puntuation={puntuation} total={total}/>
+      <Positive good={good} total={total}/>
     </div>
   )
 }
