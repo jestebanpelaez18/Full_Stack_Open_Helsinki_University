8b128c23dda6a4a8a04e588090155fd0af00f8ce
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-03-03 17:18:12 +0200

    feature(Search Filter): Added search Filter that checks for names in the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,15 @@ import Numbers from "./components/Numbers";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567", id: 0 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchItem, setSearchItem] = useState("");
+  const [Filter, setNewFilter] = useState(persons);
 
   const addNumbers = (event) => {
     event.preventDefault();
@@ -27,22 +32,40 @@ const App = () => {
         id: String(persons.length + 1),
       };
       setPersons(persons.concat(nameObjet));
+      setNewFilter(Filter.concat(nameObjet));
       setNewName("");
       setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    const searchTerm = event.target.value;
+    setNewName(searchTerm);
   };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
 
+  const handleFiltering = (event) => {
+    const searchTerm = event.target.value;
+    setSearchItem(searchTerm);
+    const filternames = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setNewFilter(filternames);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with:{" "}
+          <input value={searchItem} onChange={handleFiltering} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addNumbers}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -55,7 +78,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {Filter.map((person) => (
         <Numbers key={person.id} number={person}></Numbers>
       ))}
     </div>
