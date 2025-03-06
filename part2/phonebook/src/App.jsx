271d8a6aed537afd2533ce5a1c46acad68d25836
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-03-06 19:20:31 +0200

    feature(phonebook): Modified the initial state in order to get the data from the server using axios library

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchItem, setSearchItem] = useState("");
-  const [filtering, setNewFilter] = useState(persons);
+  const [filtering, setNewFilter] = useState([]);
+
+  useEffect(() => {
+    console.log("efect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promised fullfilled");
+      setPersons(response.data)
+      setNewFilter(response.data)
+    });
+  },[]);
 
   const addNumbers = (event) => {
     event.preventDefault();
